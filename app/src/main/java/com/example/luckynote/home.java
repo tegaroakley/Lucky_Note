2ac48b6a698bd2b6f7b78420ac7a6365ce3ac546
Tegar Oakley <tegaroakley@gmail.com>
2021-08-08 19:11:55 +0700

    update lagi

--- app/src/main/java/com/example/luckynote/home.java
+++ app/src/main/java/com/example/luckynote/home.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.example.luckynote.adapter.NoteAdapter;
@@ -30,7 +31,7 @@ public class home extends AppCompatActivity implements View.OnClickListener, Loa
     private NoteAdapter adapter;
     private NoteHelper noteHelper;
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
